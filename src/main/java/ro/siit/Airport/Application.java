a4e72cc42cae3fea6aad466b6781bff0dc92e4bb
abalanag <79378618+abalanag@users.noreply.github.com>
2021-07-15 21:34:02 +0300

    Preparing project
    - Modified gitignore so that can ignore all unwanted files
    - Added maven in our project
    - Added Spring dependencies on pom.xml
    - Prepared the structure for a Spring project

---
+++ src/main/java/ro/siit/Airport/Application.java
@@ -0,0 +1,12 @@
+package ro.siit.Airport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
